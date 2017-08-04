a0851e365932190eacef91ca8d0972957927bf92
blaec <odeskonst@yandex.ru>
2017-08-04 12:43:55 +0300

    2613.12 incl.

--- javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -1,16 +1,18 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 import com.javarush.task.task26.task2613.exception.NotEnoughMoneyException;
 
-public class LoginCommand implements Command {
+import java.util.Enumeration;
+import java.util.ResourceBundle;
 
+public class LoginCommand implements Command {
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException {
-        String creditCard = "123456789012";
-        String pin = "1234";
 
         while (true) {
 
@@ -24,16 +26,25 @@ public class LoginCommand implements Command {
                     pinCode.matches("\\d{4}") && pinCode.length() == 4) {
 
                 // Try to identify data
-                if (cardId.equals(creditCard) && cardId.equals(pin)) {
+                if (validCreditCards.containsKey(cardId) && validCreditCards.getString(cardId).equals(pinCode)) {
                     ConsoleHelper.writeMessage("Entered data is valid. ");
                     break;
                 }
-                else {
-                    ConsoleHelper.writeMessage("Invalid data. ");
 
-                    // ??? Validator requires exit at this point, i.e. user can proceed with incorrect data
-                    break;
-                }
+// Validation does not accept this method, it's not effective but works
+//                Enumeration bundleKeys = validCreditCards.getKeys();
+//                boolean accessGranted = false;
+//                while (bundleKeys.hasMoreElements()) {
+//                    String creditCard = (String) bundleKeys.nextElement();
+//                    String pin = validCreditCards.getString(creditCard);
+//
+//                    if (cardId.equals(creditCard) && pinCode.equals(pin)) {
+//                        ConsoleHelper.writeMessage("Entered data is valid. ");
+//                        accessGranted = true;
+//                        break;
+//                    }
+//                }
+//                if (accessGranted) break;
             }
             else ConsoleHelper.writeMessage("Invalid data. ");
         }
