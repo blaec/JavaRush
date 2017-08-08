62266cac52b6a88bb9c3910e0c298762c410ce5b
blaec <odeskonst@yandex.ru>
2017-08-09 00:21:27 +0300

    3410.09 done

--- javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task34.task3410.view;
 
+import com.javarush.task.task34.task3410.controller.EventListener;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
@@ -9,6 +10,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Field extends JPanel {
+    private EventListener eventListener;
     private View view;
 
     public Field(View view) throws HeadlessException {
@@ -25,5 +27,9 @@ public class Field extends JPanel {
         home.draw(g);
         box.draw(g);
         wall.draw(g);
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 }
