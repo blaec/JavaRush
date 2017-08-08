2bb51bccd6399e74742bef0fce7f288380981bd4
blaec <odeskonst@yandex.ru>
2017-08-08 23:49:40 +0300

    3410.07 done

--- javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -3,6 +3,7 @@ package com.javarush.task.task34.task3410.view;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
+import com.javarush.task.task34.task3410.model.Wall;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,11 +17,13 @@ public class Field extends JPanel {
 
     // https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html
     public void paint(Graphics g) {
-//        Player player = new Player(70, 70);
-//        Home home = new Home(70,70);
-//        Box box = new Box(70, 70);
-//        player.draw(g);
-//        home.draw(g);
-//        box.draw(g);
+        Player player = new Player(70, 70);
+        Home home = new Home(70,70);
+        Box box = new Box(70, 70);
+        Wall wall = new Wall(100,100);
+        player.draw(g);
+        home.draw(g);
+        box.draw(g);
+        wall.draw(g);
     }
 }
