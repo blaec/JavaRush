124bfb40169c8beed12fcfd7fce54c041b8a3d7d
blaec <odeskonst@yandex.ru>
2017-08-09 23:37:52 +0300

    3410.14 done

--- javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ javaRush/projects/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -5,14 +5,20 @@ import com.javarush.task.task34.task3410.model.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Set;
 
 public class Field extends JPanel {
     private EventListener eventListener;
     private View view;
 
+
     public Field(View view) throws HeadlessException {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     // https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html
@@ -29,5 +35,19 @@ public class Field extends JPanel {
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public class KeyHandler extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case (KeyEvent.VK_LEFT):    eventListener.move(Direction.LEFT);     break;
+                case (KeyEvent.VK_RIGHT):   eventListener.move(Direction.RIGHT);    break;
+                case (KeyEvent.VK_UP):      eventListener.move(Direction.UP);       break;
+                case (KeyEvent.VK_DOWN):    eventListener.move(Direction.DOWN);     break;
+                case (KeyEvent.VK_R):       eventListener.restart();                break;
+            }
+        }
     }
 }
