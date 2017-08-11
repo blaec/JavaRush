856575d19899e9307720d07e4043852dcdc27542
blaec <odeskonst@yandex.ru>
2017-08-12 01:33:23 +0300

    2113.06 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
+    public static Hippodrome game = null;
     private List<Horse> horses = null;
 
     public Hippodrome(List<Horse> horses) {
@@ -10,7 +12,10 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-
+        game = new Hippodrome(new ArrayList<>());
+        game.horses.add(new Horse("horse_1", 3, 0));
+        game.horses.add(new Horse("horse_2", 3, 0));
+        game.horses.add(new Horse("horse_3", 3, 0));
     }
 
     public List<Horse> getHorses() {
