c029ce957585ed6e4689c3fd5757cb1775ccdefe
blaec <odeskonst@yandex.ru>
2017-08-11 18:21:27 +0300

    2104 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -6,6 +6,7 @@ package com.javarush.task.task21.task2103;
 public class Solution {
     public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
         /*
+        * http://info.javarush.ru/JavaRush_tasks_discussion/2014/04/10/package-com-javarush-test-level21-lesson02-task03-.html
         * (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
         * c && (a && b && !d) || !a || !b || d
         */
---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object n) {
+        if (n == null) return false;
+
+        if (!(n instanceof Solution)) return false;
+
+        if (n == this) return  true;
+
+        Solution solution = (Solution)n;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int firstHash = first == null ? 0 : first.hashCode();
+        int lastHash = last == null ? 0 : last.hashCode();
+        return 31 * firstHash + lastHash;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
