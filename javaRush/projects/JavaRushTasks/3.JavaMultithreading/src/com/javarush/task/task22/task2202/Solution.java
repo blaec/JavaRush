8b83433ae7e853816e8da66abe16f4d998e7e22c
blaec <odeskonst@yandex.ru>
2017-08-12 14:26:45 +0300

    2202 done

---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task22.task2202;
+
+
+/*
+Найти подстроку
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+    }
+
+    public static String getPartOfString(String string) {
+
+        // validate string
+        if (string == null || string.isEmpty()) throw new TooShortStringException();
+
+        // Count spaces
+        String[] tokens = string.split(" ");
+        if (tokens.length < 5) throw new TooShortStringException();
+
+        // Create output
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 5; i++) {
+            sb.append(tokens[i]).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static class TooShortStringException extends RuntimeException {
+    }
+}
