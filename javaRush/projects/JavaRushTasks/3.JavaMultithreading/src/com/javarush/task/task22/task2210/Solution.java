2e027a899aca7fc43af3605c4196fe0cc8aaed9a
blaec <odeskonst@yandex.ru>
2017-08-12 23:51:34 +0300

    2210 done

---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+StringTokenizer
+*/
+public class Solution {
+    public static void main(String[] args) {
+        String[] test = getTokens("level22.lesson13.task01", ".");
+        System.out.println(Arrays.toString(test));
+    }
+    public static String [] getTokens(String query, String delimiter) {
+        List<String> temp = new ArrayList<>();
+
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        while (tokenizer.hasMoreTokens()) {
+            temp.add(tokenizer.nextToken());
+        }
+
+
+
+        String[] output = new String[temp.size()];
+        return temp.toArray(output);
+
+        /*
+        * Better option
+        * StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        * String[] output = new String[tokenizer.countTokens()];
+        *
+        * for (int i=0; tokenizer.hasMoreElements();i++) {
+        *   output[i] = tokenizer.nextToken();
+        * }
+        *
+        * return output;
+        */
+
+    }
+}
