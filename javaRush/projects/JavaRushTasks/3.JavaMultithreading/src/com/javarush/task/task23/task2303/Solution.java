ab4d2fb06973cd17893b43d84fca2b59cf4ddb1d
blaec <odeskonst@yandex.ru>
2017-08-15 00:52:12 +0300

    2303 done

---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task23/task2303/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task23.task2303;
+
+/* 
+Запрети создание экземпляров класса
+*/
+public class Solution {
+
+    public abstract static class Listener {
+        public void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+
+        private Listener() {
+        }
+    }
+
+    public static void main(String[] args) {
+//        Listener listener = new Listener();
+//        System.out.println(listener.getClass());
+    }
+}
