83c608765fbd236f4d9d4ca0f4e34504b084e8ec
blaec <odeskonst@yandex.ru>
2017-08-19 00:30:54 +0300

    2413.13 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
     @Override
     public void draw(Canvas canvas) {
 
@@ -11,7 +17,28 @@ public class Ball extends BaseObject {
 
     }
 
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        isFrozen = true;
+    }
+
+
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
     }
 }
