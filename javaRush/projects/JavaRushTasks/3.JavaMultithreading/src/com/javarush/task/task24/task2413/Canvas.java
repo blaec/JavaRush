89e4967d08a221f75b82cae9ebef84ec5acdb26b
blaec <odeskonst@yandex.ru>
2017-08-19 01:29:25 +0300

    2413.20 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,71 +1,86 @@
 package com.javarush.task.task24.task2413;
 
+/**
+ * Класс-холст для отрисовки.
+ */
 public class Canvas {
-    private int height;
+    //ширина и высота
     private int width;
+    private int height;
+    //матрица, где рисуем. символ - это цвет.
     private char[][] matrix;
 
-    // order important for validation
     public Canvas(int width, int height) {
-        this.height = height;
         this.width = width;
-
-        // instead of this line can execute clear() method
-        matrix = new char[height + 2][ width + 2];
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
         this.height = height;
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    /**
+     * Очищаем холст
+     */
+    void clear() {
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    /**
+     * Печатаем переданную фигуру в указанных координатах цветом c.
+     * Если переданный массив содержит единицы, то на холсте им будут соответствовать символы - с.
+     */
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 1)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+    /**
+     * Ставим одну точку на холсте с координатами (x,y) и цветом - c.
+     */
+    void setPoint(double x, double y, char c) {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (y0 < 0 || y0 >= matrix.length) return;
+        if (x0 < 0 || x0 >= matrix[y0].length) return;
+
+        matrix[y0][x0] = c;
+    }
+
+    /**
+     * Печатаем содержимое холста на экран.
+     */
+    void print() {
+        System.out.println();
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     public int getWidth() {
         return width;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
+    public int getHeight() {
+        return height;
     }
 
     public char[][] getMatrix() {
         return matrix;
-    }
-
-    public void setMatrix(char[][] matrix) {
-        this.matrix = matrix;
-    }
-
-    // round the double and then cast int
-    public void setPoint(double x, double y, char c) {
-        int intX = (int) Math.round(x);
-        int intY = (int) Math.round(y);
-        if (!(intX < 0 || intY < 0 || intX > matrix[0].length || intY > matrix.length)) {
-            matrix[intY][intX] = c;
-        }
-    }
-
-    public void drawMatrix(double x, double y, int[][] matrix, char c) {
-
-        for (int my = 0; my < matrix.length; my++) {
-            for (int mx = 0; mx < matrix[my].length; mx++) {
-                setPoint(x + mx, y + my, c);
-            }
-        }
-    }
-
-    public void clear() {
-        matrix = new char[height + 2][ width + 2];
-    }
-
-    public void print() {
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                System.out.print(matrix[y][x]);
-            }
-            System.out.println();
-        }
     }
 }
