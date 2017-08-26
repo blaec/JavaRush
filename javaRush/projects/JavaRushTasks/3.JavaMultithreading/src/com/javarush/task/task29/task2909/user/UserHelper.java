4452f20b870ada0a55639edc33585200bd69d46d
blaec <odeskonst@yandex.ru>
2017-08-27 01:19:40 +0300

    2909.14 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
@@ -6,30 +6,15 @@ public class UserHelper {
     private User userAnya = new User("Аня", "Смирнова", 10);
     private User userRoma = new User("Рома", "Виноградов", 30);
 
-    private boolean isManAnya = false;
-    private boolean isManRoma = true;
+//    private boolean isManAnya = false;
+//    private boolean isManRoma = true;
 
     public void printUsers() {
         userAnya.printInfo();
         userAnya.printAdditionalInfo();
         userRoma.printInfo();
         userRoma.printAdditionalInfo();
-//        System.out.println("Имя: " + userAnya.getName());
-//        System.out.println("Фамилия: " + userAnya.getSurname());
-//        printAdditionalInfo(userAnya);
-//
-//        System.out.println("Имя: " + userRoma.getName());
-//        System.out.println("Фамилия: " + userRoma.getSurname());
-//        printAdditionalInfo(userRoma);
     }
-
-
-//    private boolean ageLessThan16(User user) {
-//        if (user.getAge() < 16) {
-//            return true;
-//        }
-//        return false;
-//    }
 
     public int calculateAverageAge() {
         User userUra = new User("Юра", "Карп", 28);
@@ -48,7 +33,7 @@ public class UserHelper {
     }
 
     public String getBossName(User user) {
-        Work work = user.getWork();
-        return work.getBoss();
+//        Work work = user.getWork();
+        return user.getBoss();
     }
 }
