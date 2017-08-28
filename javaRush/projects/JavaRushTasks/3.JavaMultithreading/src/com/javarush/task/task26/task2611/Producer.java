9be64f9cd5453d403df9690562d49fcea37f472e
blaec <odeskonst@yandex.ru>
2017-08-28 21:22:10 +0300

    2611 done

---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task26.task2611;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Producer implements Runnable{
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while (true) {
+            try {
+                i++;
+                map.put(i + "", "Some text for " + i);
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+                break;  // accepted without break, but it should exit eternal loop
+            }
+        }
+    }
+}
