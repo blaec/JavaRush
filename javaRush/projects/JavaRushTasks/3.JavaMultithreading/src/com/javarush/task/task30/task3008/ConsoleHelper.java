c1a869c775671e86b32244ef4486be25867ea762
blaec <odeskonst@yandex.ru>
2017-08-29 20:48:07 +0300

    3008.02 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,42 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String output = null;
+
+        while (true) {
+            try {
+                output = bufferedReader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return output;
+    }
+
+    public static int readInt() {
+        int output = 0;
+
+        while (true) {
+            try {
+                output = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+
+        return output;
+    }
 }
