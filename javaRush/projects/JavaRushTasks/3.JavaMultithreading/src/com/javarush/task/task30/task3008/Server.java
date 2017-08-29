e639560c9dc7e3b27150d65e50dc2bb48d8bd628
blaec <odeskonst@yandex.ru>
2017-08-29 21:57:21 +0300

    3008.06 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,34 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server started...");
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                Handler handler = new Handler(clientSocket);
+                handler.start();
+            } catch (Exception e) {
+                serverSocket.close();
+                System.out.println("Exception");
+                break;
+            }
+        }
     }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+
 }
