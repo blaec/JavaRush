3a96821ada18bc1e04039129c01c739c4b6177da
blaec <odeskonst@yandex.ru>
2017-08-30 23:18:27 +0300

    3008.17 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -6,6 +6,7 @@ import com.javarush.task.task30.task3008.Message;
 import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
     protected Connection connection;
@@ -129,6 +130,16 @@ public class Client {
                     isLoop = false;
                     throw new IOException("Unexpected MessageType");
                 }
+            }
+        }
+
+        public void run() {
+            try {
+                connection = new Connection(new Socket(getServerAddress(), getServerPort()));
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException | ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
             }
         }
     }
