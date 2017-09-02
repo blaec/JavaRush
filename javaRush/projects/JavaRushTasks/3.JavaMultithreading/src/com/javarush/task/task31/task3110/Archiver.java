36b497a8302ddc2278d5251689ae6b762d1f2c62
blaec <odeskonst@yandex.ru>
2017-09-02 20:56:12 +0300

    3110.08 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,23 +1,50 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
 import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        Constructor<CommandExecutor> constructor = CommandExecutor.class.getDeclaredConstructor(new Class[0]);
+//        constructor.setAccessible(true);
+//        CommandExecutor commandExecutor = constructor.newInstance(new Object[0]);
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT);
+    }
 
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(bufferedReader.readLine()));
-        zipFileManager.createZip(Paths.get(bufferedReader.readLine()));
-//        ZipFileManager zipFileManager = new ZipFileManager(Paths.get("C:\\CCC Images\\file.zip"));
-//        zipFileManager.createZip(Paths.get("C:\\CCC Images\\file.csv"));
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                                   "0 - упаковать файлы в архив\n" +
+                                   "1 - добавить файл в архив\n" +
+                                   "2 - удалить файл из архива\n" +
+                                   "3 - распаковать архив\n" +
+                                   "4 - просмотреть содержимое архива\n" +
+                                   "5 – выход");
+        int operationId = ConsoleHelper.readInt();
+//        Operation result = null;
+//        for (Operation operation : Operation.values()) {
+//            if (operation.ordinal() == operationId) {
+//                result = operation;
+//                break;
+//            }
+//        }
+//        if (result == null) throw new IOException();
 
-        bufferedReader.close();
-
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        return Operation.values()[operationId];
     }
 }
