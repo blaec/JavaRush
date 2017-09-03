b429014da2ab97696b53947ddbebca0b0b12929c
blaec <odeskonst@yandex.ru>
2017-09-03 07:20:49 +0300

    3110.09 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,19 +1,12 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Constructor;
-import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
-//        Constructor<CommandExecutor> constructor = CommandExecutor.class.getDeclaredConstructor(new Class[0]);
-//        constructor.setAccessible(true);
-//        CommandExecutor commandExecutor = constructor.newInstance(new Object[0]);
+    public static void main(String[] args) throws IOException {
+
         Operation operation = null;
         do {
             try {
@@ -24,27 +17,21 @@ public class Archiver {
             } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
+
         } while (operation != Operation.EXIT);
     }
 
-    public static Operation askOperation() throws IOException {
-        ConsoleHelper.writeMessage("Выберите операцию:\n" +
-                                   "0 - упаковать файлы в архив\n" +
-                                   "1 - добавить файл в архив\n" +
-                                   "2 - удалить файл из архива\n" +
-                                   "3 - распаковать архив\n" +
-                                   "4 - просмотреть содержимое архива\n" +
-                                   "5 – выход");
-        int operationId = ConsoleHelper.readInt();
-//        Operation result = null;
-//        for (Operation operation : Operation.values()) {
-//            if (operation.ordinal() == operationId) {
-//                result = operation;
-//                break;
-//            }
-//        }
-//        if (result == null) throw new IOException();
 
-        return Operation.values()[operationId];
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
