3586aa4877a63c58c983e3cae0fb4b69849ea834
blaec <odeskonst@yandex.ru>
2017-09-03 23:33:54 +0300

    3110.12 done

---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task31.task3110;
+
+public class FileProperties {
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+    public long getCompressionRatio() {
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        String output = String.format("%s", name);
+        if (size > 0) output = String.format("%s\t%d Kb (%d Kb) сжатие: %d%%", name, size / 1024, compressedSize / 1024, getCompressionRatio());
+
+        return output;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+}
