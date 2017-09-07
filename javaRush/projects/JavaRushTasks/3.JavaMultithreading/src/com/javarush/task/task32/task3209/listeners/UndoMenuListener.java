cd46ad760159a4a66ce3b7aa4cd1443d8efbcb8d
blaec <odeskonst@yandex.ru>
2017-09-07 23:34:38 +0300

    3209.10 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/UndoMenuListener.java
@@ -7,16 +7,24 @@ import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
 public class UndoMenuListener implements MenuListener {
+    private View view;
+    private JMenuItem undoMenuItem = new JMenuItem("Отменить");
+    private JMenuItem redoMenuItem = new JMenuItem("Вернуть");
+
     public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+        this.view = view;
+        this.undoMenuItem = undoMenuItem;
+        this.redoMenuItem = redoMenuItem;
     }
 
     @Override
     public void menuSelected(MenuEvent e) {
-
+        undoMenuItem.setEnabled(view.canUndo());
+        redoMenuItem.setEnabled(view.canRedo());
     }
 
     @Override
-    public void menuDeselected(MenuEvent e) {
+    public void menuDeselected(MenuEvent menuEvent) {
 
     }
 
