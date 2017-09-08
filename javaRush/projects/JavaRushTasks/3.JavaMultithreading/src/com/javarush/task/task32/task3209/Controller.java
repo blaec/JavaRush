62549d708282fef8e22c61d1b4d51eed57ea7338
blaec <odeskonst@yandex.ru>
2017-09-08 15:14:33 +0300

    3209.15 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -32,5 +35,15 @@ public class Controller {
 
     public HTMLDocument getDocument() {
         return document;
+    }
+
+    public void resetDocument() {
+        if (document != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
     }
 }
