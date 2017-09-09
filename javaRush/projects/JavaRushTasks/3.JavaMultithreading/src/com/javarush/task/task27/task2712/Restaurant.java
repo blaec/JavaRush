5bd14a15fa2cd1f260481441d91cf8e44e4cf09d
blaec <odeskonst@yandex.ru>
2017-09-09 14:28:18 +0300

    2712.04 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,15 +1,17 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
-import com.javarush.task.task27.task2712.kitchen.Order;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
+// notifyObservers(arg) triggers appropriate update method
 public class Restaurant {
     public static void main(String[] args) {
         Cook cook = new Cook("cooK");
         Tablet tablet = new Tablet(5);
         tablet.addObserver(cook);
-        Order order = tablet.createOrder();
-        cook.update(tablet, order);
+        Waiter waiter = new Waiter();
+        cook.addObserver(waiter);
+        tablet.createOrder();
     }
 }
 
