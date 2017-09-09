5b359ff3d962a307ce82fff04cceef5daeb400ff
blaec <odeskonst@yandex.ru>
2017-09-09 14:15:37 +0300

    2712.03 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,10 +1,15 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Order;
+
 public class Restaurant {
     public static void main(String[] args) {
+        Cook cook = new Cook("cooK");
         Tablet tablet = new Tablet(5);
-        tablet.createOrder();
-        System.out.println(tablet.toString());;
+        tablet.addObserver(cook);
+        Order order = tablet.createOrder();
+        cook.update(tablet, order);
     }
 }
 
--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,30 +3,35 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable{
     final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
-//    private Order order;
 
     public Tablet(int number) {
         this.number = number;
     }
 
-    public void createOrder() {
+    public Order createOrder() {
+        Order order = null;
+
         try {
-            new Order(this);
-//            order = new Order(this);
+            order = new Order(this);
+            ConsoleHelper.writeMessage(order.toString());
+            setChanged();
+            notifyObservers(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
+
+        return order;
     }
 
     @Override
     public String toString() {
         return String.format("Tablet{number=%d}", number);
-//        return String.format("Your order: [%s] of Tablet{number=%d}", order.toString() ,number);
     }
 }
