3d6c74f30d375d366f3363676f526a5089857453
blaec <odeskonst@yandex.ru>
2017-09-11 01:41:24 +0300

    2712.12 done

---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/CookedOrderEventDataRow.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+import com.javarush.task.task27.task2712.kitchen.Dish;
+
+import java.util.Date;
+import java.util.List;
+
+public class CookedOrderEventDataRow implements EventDataRow{
+    private String tabletName;          // tablet name
+    private String cookName;            // cook name
+    private int cookingTimeSeconds;     // total order execution time
+    private List<Dish> cookingDishs;    // ordered dishes' list
+    private Date currentDate;
+
+    public CookedOrderEventDataRow(String tabletName, String cookName, int cookingTimeSeconds, List<Dish> cookingDishs) {
+        this.tabletName = tabletName;
+        this.cookName = cookName;
+        this.cookingTimeSeconds = cookingTimeSeconds;
+        this.cookingDishs = cookingDishs;
+        currentDate = new Date();
+    }
+}
---
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/event/VideoSelectedEventDataRow.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task27.task2712.statistic.event;
+
+import com.javarush.task.task27.task2712.ad.Advertisement;
+
+import java.util.Date;
+import java.util.List;
+
+public class VideoSelectedEventDataRow implements EventDataRow {
+    private List<Advertisement> optimalVideoSet;    // videos' list to display
+    private long amount;                            // cash in cents
+    private int totalDuration;                      // ads total duration
+    private Date currentDate;
+
+    public VideoSelectedEventDataRow(List<Advertisement> optimalVideoSet, long amount, int totalDuration) {
+        this.optimalVideoSet = optimalVideoSet;
+        this.amount = amount;
+        this.totalDuration = totalDuration;
+        currentDate = new Date();
+    }
+}
