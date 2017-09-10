5fc766ab66b1be5c85bf8d988760071c23b00b21
blaec <odeskonst@yandex.ru>
2017-09-10 21:03:31 +0300

    2712.07 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,16 +1,31 @@
 package com.javarush.task.task27.task2712.ad;
 
-public class AdvertisementStorage {
-    private static AdvertisementStorage instance = null;
+import java.util.ArrayList;
+import java.util.List;
 
-    protected AdvertisementStorage() {
-        // Exists only to defeat instantiation.
-    }
+public class AdvertisementStorage {
+    private static AdvertisementStorage ourInstance = new AdvertisementStorage();
+    private final List videos = new ArrayList();
 
     public static AdvertisementStorage getInstance() {
-        if(instance == null) {
-            instance = new AdvertisementStorage();
-        }
-        return instance;
+        Object someContent = new Object();
+        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); // 3 min
+        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //15 min
+        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //10 min
+        return ourInstance;
+    }
+
+    private AdvertisementStorage() {
+
+
+
+    }
+
+    public List list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }
