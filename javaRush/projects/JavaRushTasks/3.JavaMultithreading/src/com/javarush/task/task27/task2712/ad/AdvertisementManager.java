183ca62b7262ae0152e3d9052f55e1f4b59fb7a5
blaec <odeskonst@yandex.ru>
2017-09-11 01:05:16 +0300

    2712.10 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -2,6 +2,11 @@ package com.javarush.task.task27.task2712.ad;
 
 import com.javarush.task.task27.task2712.ConsoleHelper;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -10,16 +15,39 @@ public class AdvertisementManager {
         this.timeSeconds = timeSeconds;
     }
 
-    public void processVideos() {
-        // get list of available videos [max profit]
-        if (storage.list().isEmpty()) {
+    public void processVideos(){
+        List<Advertisement> ads = storage.list();
+        Collections.sort(ads, new Comparator<Advertisement>() {
+            @Override
+            public int compare(Advertisement o1, Advertisement o2) {
+                int result = Long.compare(o1.getAmountPerOneDisplaying(), o2.getAmountPerOneDisplaying());
+                if (result != 0) return -result;
+
+                long secCost1 = 1000 * o1.getAmountPerOneDisplaying() / o1.getDuration();
+                long secCost2 = 1000 * o2.getAmountPerOneDisplaying() / o2.getDuration();
+
+                return Long.compare(secCost1, secCost2);
+            }
+        });
+
+        int adsTime = timeSeconds;
+        for (Advertisement ad : ads) {
+            if (adsTime < ad.getDuration()) {
+                continue;
+            }
+
+            long secCost = 1000 * ad.getAmountPerOneDisplaying() / ad.getDuration();
+            ConsoleHelper.writeMessage(String.format("%s is displaying... %d, %d", ad.getName(),
+                                                                                   ad.getAmountPerOneDisplaying(),
+                                                                                   secCost));
+
+            adsTime -= ad.getDuration();
+            ad.revalidate();
+        }
+
+        if (adsTime == timeSeconds) {
             throw new NoVideoAvailableException();
         }
-        /** Display all ads to display sorted by
-         * 1st key: video cost (descending) [50 - 50 cents)
-         * 2nd key: cost of 1 sec (ascending) [277 - 0.277 cent]
-         * like
-         * First Video is displaying... 50, 277
-         */
     }
+
 }
--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -5,19 +5,20 @@ import java.util.List;
 
 public class AdvertisementStorage {
     private static AdvertisementStorage ourInstance = new AdvertisementStorage();
-    private final List videos = new ArrayList();
+    private final List<Advertisement> videos = new ArrayList();
 
     public static AdvertisementStorage getInstance() {
-        Object someContent = new Object();
-        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); // 3 min
-        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //15 min
-        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //10 min
         return ourInstance;
     }
 
-    private AdvertisementStorage() {}
+    private AdvertisementStorage() {
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
+    }
 
-    public List list() {
+    public List<Advertisement> list() {
         return videos;
     }
 
