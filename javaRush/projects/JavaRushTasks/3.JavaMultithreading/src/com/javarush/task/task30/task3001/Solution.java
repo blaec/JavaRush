29b55199eb4c2c8bc963e058267a468bb99915d7
blaec <odeskonst@yandex.ru>
2017-09-13 19:54:01 +0300

    3001 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3001/Solution.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3001/Solution.java
@@ -1,10 +1,13 @@
 package com.javarush.task.task30.task3001;
 
-/* 
+import java.math.BigInteger;
+
+/*
 Конвертер систем счислений
 */
 public class Solution {
     public static void main(String[] args) {
+
         Number number = new Number(NumerationSystemType._10, "6");
         Number result = convertNumberToOtherNumerationSystem(number, NumerationSystemType._2);
         System.out.println(result);    //expected 110
@@ -16,6 +19,14 @@ public class Solution {
 
     public static Number convertNumberToOtherNumerationSystem(Number number, NumerationSystem expectedNumerationSystem) {
         //напишите тут ваш код
-        return null;
+        // Convert into DEC even if it's already DEC
+//        int dec = Integer.parseInt(number.getDigit(), number.getNumerationSystem().getNumerationSystemIntValue());
+        BigInteger dec = new BigInteger(number.getDigit(), number.getNumerationSystem().getNumerationSystemIntValue());
+
+        // Convert to another base
+//        String output = Integer.toString(dec, expectedNumerationSystem.getNumerationSystemIntValue());
+        String output = (new BigInteger(dec.toString())).toString(expectedNumerationSystem.getNumerationSystemIntValue());
+
+        return new Number(expectedNumerationSystem, output.toString());
     }
 }
