6775c041b34eaf7f2c2f0cc602e2ef6db8dc39bf
blaec <odeskonst@yandex.ru>
2017-09-15 21:18:28 +0300

    3513.03 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -2,4 +2,15 @@ package com.javarush.task.task35.task3513;
 
 // Contain game logic and save game field
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[0].length; j++) {
+                gameTiles[j][i] = new Tile();
+            }
+        }
+    }
 }
