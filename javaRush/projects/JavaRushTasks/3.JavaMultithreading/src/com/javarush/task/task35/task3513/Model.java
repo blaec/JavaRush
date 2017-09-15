b288b8949db3788adcdfe307b4326a64cf98993e
blaec <odeskonst@yandex.ru>
2017-09-15 23:23:23 +0300

    3513.04 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,16 +1,44 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Contain game logic and save game field
 public class Model {
     private static final int FIELD_WIDTH = 4;
-    private Tile[][] gameTiles;
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
     public Model() {
-        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        resetGameTiles();
+    }
+
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+
+        Tile rndTile = emptyTiles.get((int) (Math.random()*emptyTiles.size()));
+        rndTile.value = Math.random() < 0.9 ? 2 : 4;
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> emptyTiles = new ArrayList<>();
+
         for (int i = 0; i < gameTiles.length; i++) {
             for (int j = 0; j < gameTiles[0].length; j++) {
-                gameTiles[j][i] = new Tile();
+                if (gameTiles[i][j].isEmpty())
+                    emptyTiles.add(gameTiles[i][j]);
             }
         }
+
+        return emptyTiles;
+    }
+
+    void resetGameTiles() {
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[0].length; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+        addTile();
+        addTile();
     }
 }
