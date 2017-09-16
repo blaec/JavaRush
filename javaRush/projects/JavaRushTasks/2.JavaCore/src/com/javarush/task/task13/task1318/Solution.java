bcd01a33f6647240d1442d6a29e1418ff1c1ca94
blaec <odeskonst@yandex.ru>
2017-09-16 21:52:53 +0300

    1318 done

--- javaRush/projects/JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ javaRush/projects/JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task13.task1318;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.io.FileInputStream;
@@ -12,5 +13,38 @@ import java.io.FileInputStream;
 public class Solution {
     public static void main(String[] args) {
         // напишите тут ваш код
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String fileName = null;
+
+        try {
+            fileName = bufferedReader.readLine();
+
+
+            Scanner scanner = new Scanner(new FileInputStream(fileName));
+
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+
+            // Validator not accept char-reading
+//            FileInputStream fis = new FileInputStream(fileName);
+
+//            while (fis.available() > 0) {
+//                byte[] buffer = new byte[fis.available()];
+//                int count = fis.read(buffer);
+//
+//                for (int i = 0; i < buffer.length; i++) {
+//                    System.out.print((char) buffer[i]);
+//                }
+//            }
+
+//            fis.close();
+
+            scanner.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
