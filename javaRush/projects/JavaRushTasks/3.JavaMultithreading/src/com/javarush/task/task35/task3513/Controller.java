38dc82d4154183d235cd8b1917955bd4e89021be
blaec <odeskonst@yandex.ru>
2017-09-16 17:14:03 +0300

    3513.09 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,11 +1,19 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 // Listens to keystrokes
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+
+    private final static int WINNING_TILE = 2048;
+
+    public Controller(Model model) {
+        this.model = model;
+        this.view = new View(this);
+    }
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -13,5 +21,30 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameLost = false;
+        view.isGameWon = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE)   resetGame();
+        if (!model.canMove())                       view.isGameLost = true;
+        if (!view.isGameLost && !view.isGameWon) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:  model.left();   break;
+                case KeyEvent.VK_RIGHT: model.right();  break;
+                case KeyEvent.VK_UP:    model.up();     break;
+                case KeyEvent.VK_DOWN:  model.down();   break;
+            }
+        }
+        if (model.maxTile == WINNING_TILE)          view.isGameWon = true;
+        view.repaint();
+
+        super.keyPressed(e);
     }
 }
