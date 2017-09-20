55a9803d9f4a675b8ee56aa619a963e9922c5951
blaec <odeskonst@yandex.ru>
2017-09-20 18:25:52 +0300

    2512 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -1,23 +1,32 @@
 package com.javarush.task.task25.task2512;
 
-/* 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
 Живем своим умом
 */
 public class Solution implements Thread.UncaughtExceptionHandler {
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
+        List<String> messages = new ArrayList<>();
+
         t.interrupt();
+
+        while (e != null) {
+            messages.add(e.toString());
+            e = e.getCause();
+        }
+
+        for (int i = messages.size() - 1; i >= 0; i--) {
+            System.out.println(messages.get(i));
+        }
     }
 
     public static void main(String[] args) {
         Thread thread = new Thread();
-        thread.start();
-        thread.getUncaughtExceptionHandler();
-        try {
-            throw new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI")));
-        } catch (Exception e) {
-//            Solution.uncaughtException(thread, e);
-        }
+        Solution solution = new Solution();
+        solution.uncaughtException(thread, new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI"))));
     }
 }
