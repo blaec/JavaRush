1ad5bccc3f9f4c35712188252d07033b820ec759
blaec <odeskonst@yandex.ru>
2017-09-20 17:44:20 +0300

    2212 done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -1,47 +1,61 @@
 package com.javarush.task.task22.task2212;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /*
 Проверка номера телефона
 */
 public class Solution {
     public static boolean checkTelNumber(String telNumber) {
 
-        // 7. Ends with digit
-//        if (telNumber.matches("\\d+$"))    return true;
-//        if (telNumber.matches("\\+\\d{12}"))    return true;
-//        if (telNumber.matches("[(]\\d{9}") || telNumber.matches("\\d{10}"))
-//                                                       return true;
-//        if (telNumber.matches("[-]{0,2}"))    return true;
-//
-//        if (telNumber.matches(""))    return true;
-//        if (telNumber.matches(""))    return true;
+        // If null string or length less then 10
+        if (telNumber == null || telNumber.length() < 10){
+            System.out.print("first - ");
+            return false;
+        }
 
-        // 6. No letters
-        if (telNumber.matches("[^a-zA-Z]*"))    return true;
+        // If starts with + then it contains 12 digits
+        if (telNumber.matches("^\\+[0-9]{12}")){
+            System.out.print("^\\+[0-9]{12} - ");
+            return true;
+        }
 
+        // Parenthes contain 3 digits
+        // but in this case + 2 digits (3 digits) 7 digits [total 12 digits]
+        if (telNumber.matches("^\\+[0-9]{2}\\([0-9]{3}\\)\\d{7}")){
+            System.out.print("^\\+[0-9]{2}\\([0-9]{3}\\)\\d{7} - ");
+            return true;
+        }
+
+        // Can contain 0-2 "-", can't go together
+        // not exactly + 8 digits - 2 digits - 2 digits
+        if(telNumber.matches("^\\+[0-9]{8}-\\d{2}-\\d{2}")){
+            System.out.print("^\\+[0-9]{8}-\\d{2}-\\d{2} - ");
+            return true;
+        }
+
+        // 6 digits - 4 digits
+        if (telNumber.matches("^[0-9]{6}-[0-9]{4}")){
+            System.out.print("^[0-9]{6}-[0-9]{4} - ");
+            return true;
+        }
+
+        // Number does not contain letters
+        // Number ends with digit
+        System.out.print("last - ");
         return false;
+
+        // If starts with digit or ( - then it contains 10 digits
+        // Can contain pair of parenthes, which always to the left of "-"
     }
 
     public static void main(String[] args) {
-        List<String> test = new ArrayList<>();
-        test.add("+380501234567");
-        test.add("+38(050)1234567");
-        test.add("+38050123-45-67");
-        test.add("050123-4567");
-        test.add("+38)050(1234567");
-        test.add("+38(050)1-23-45-6-7");
-        test.add("050ххх4567");
-        test.add("050123456");
-        test.add("(0)501234567");
-        test.add("0123456789");
-        test.add("abc");
-
-        for (String phone : test){
-            System.out.println(phone + " - " + checkTelNumber(phone));
-        }
-
+        System.out.println("+380501234567 " + checkTelNumber("+380501234567"));
+        System.out.println("+38(050)1234567 " + checkTelNumber("+38(050)1234567"));
+        System.out.println("+38050123-45-67 " + checkTelNumber("+38050123-45-67"));
+        System.out.println("050123-4567 " + checkTelNumber("050123-4567"));
+        System.out.println("+38)050(1234567 " + checkTelNumber("+38)050(1234567"));
+        System.out.println("+38(050)1-23-45-6-7 " + checkTelNumber("+38(050)1-23-45-6-7"));
+        System.out.println("050ххх4567 " + checkTelNumber("050ххх4567"));
+        System.out.println("050123456 " + checkTelNumber("050123456"));
+        System.out.println("(0)501234567 " + checkTelNumber("(0)501234567"));
     }
 }
