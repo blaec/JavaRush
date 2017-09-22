4e322ffebf89e5b79e8e6d577836b4cd8d3b62db
blaec <odeskonst@yandex.ru>
2017-09-22 18:42:44 +0300

    2712.16- done

--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/DirectorTablet.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/DirectorTablet.java
@@ -1,9 +1,45 @@
 package com.javarush.task.task27.task2712;
 
-public class DirectorTablet {
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
 
-    public void printAdvertisementProfit() {}
-    public void printCookWorkloading() {}
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+public class DirectorTablet {
+    DateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+    public void printAdvertisementProfit() {
+        Map<Date, Double> data = StatisticManager.getInstance().getVideoDailyProfit();
+        double total = 0;
+
+        for (Date date : data.keySet()) {
+            double profit = data.get(date);
+            total += profit;
+
+            ConsoleHelper.writeMessage(String.format("%s - %.2f", df.format(date), profit));
+        }
+        if (total > 0) ConsoleHelper.writeMessage(String.format("Total - %.2f", total));
+    }
+
+    public void printCookWorkloading() {
+        Map<Date, Map<String, Integer>> data = StatisticManager.getInstance().getCookTime();
+
+        for (Date date : data.keySet()) {
+            Map<String, Integer> cooks = data.get(date);
+
+            ConsoleHelper.writeMessage(df.format(date));
+            for (String cook : cooks.keySet()) {
+                int time = cooks.get(cook);
+
+                if (time > 0) ConsoleHelper.writeMessage(String.format("%s - %d min",cook, time));
+            }
+
+        }
+    }
+
     public void printActiveVideoSet() {}
     public void printArchivedVideoSet() {}
 }
--- javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ javaRush/projects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,9 +1,14 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.statistic.event.CookedOrderEventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
 import com.javarush.task.task27.task2712.statistic.event.EventType;
+import com.javarush.task.task27.task2712.statistic.event.VideoSelectedEventDataRow;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class StatisticManager {
@@ -25,6 +30,72 @@ public class StatisticManager {
         cooks.add(cook);
     }
 
+    public Map<Date, Double> getVideoDailyProfit() {
+        Map<Date, Double> ads = new TreeMap<>(Collections.reverseOrder());
+
+        List<EventDataRow> videos = statisticStorage.get(EventType.SELECTED_VIDEOS);
+        for (EventDataRow video : videos) {
+            VideoSelectedEventDataRow nextVideo = (VideoSelectedEventDataRow) video;
+            Date date = getDateWithoutTime(nextVideo.getDate());
+            double newValue = 0.01d * (double)nextVideo.getAmount();
+            double value = newValue;
+
+            if (ads.containsKey(date)) {
+                value = ads.get(date) + newValue;
+            }
+            ads.put(date, value);
+        }
+
+        return ads;
+    }
+
+    private Date getDateWithoutTime(Date input) {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date output = null;
+
+        try {
+            output = formatter.parse(formatter.format(input));
+        } catch (ParseException e) {}
+
+        return output;
+    }
+
+    public Map<Date, Map<String, Integer>> getCookTime() {
+        Map<Date, Map<String, Integer>> dateMap = new TreeMap<>(Collections.reverseOrder());
+
+
+        List<EventDataRow> orders = statisticStorage.get(EventType.COOKED_ORDER);
+        for (EventDataRow order : orders) {
+            Map<String, Integer> cookMap = new TreeMap<>();
+            CookedOrderEventDataRow nextOrder = (CookedOrderEventDataRow) order;
+            Date date = getDateWithoutTime(nextOrder.getDate());
+            int time = roundTime(nextOrder.getTime());
+            int value = time;
+            String cook = nextOrder.getCookName();
+
+            if (dateMap.containsKey(date)) {
+                cookMap = dateMap.get(date);
+
+                if (cookMap.containsKey(cook)) {
+                    value = cookMap.get(cook) + time;
+                }
+            }
+            cookMap.put(cook, value);
+            dateMap.put(date, cookMap);
+        }
+
+        return dateMap;
+    }
+
+    private int roundTime(int time) {
+        int output = 0;
+
+        if (time != 0)
+            output = (time % 60 == 0) ? time / 60 : time / 60 + 1;
+
+        return output;
+    }
+
     private class StatisticStorage {
         private Map<EventType, List<EventDataRow>> storage;
 
@@ -40,6 +111,10 @@ public class StatisticManager {
             List<EventDataRow> value = storage.get(key);
             value.add(data);
             storage.put(key, value);
+        }
+
+        private List<EventDataRow> get(EventType key) {
+            return storage.get(key);
         }
     }
 }
